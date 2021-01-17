e281c66f5a6d246a645843b4dcde9df45741d907
loremZyn14 <yasuaimo@gmail.com>
2021-01-17 14:41:35 +0800

    add route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PostsComponent } from './components/post/posts/posts.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path : 'posts' , component : PostsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
